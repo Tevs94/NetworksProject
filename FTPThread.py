6cebd77fa803c78993dfa76694cf6a496eb56c8b
Tev <tevia.sapire@gmail.com>
2018-03-21 18:04:29 +0200

    Added Response Codes to Server

--- FTPThread.py
+++ FTPThread.py
@@ -7,42 +7,59 @@ class FTPThread(threading.Thread):
         self.threadID = threadID
         self.connectionSocket = connectionSocket
         self.quitting = False
+        self.SendReply(220)
         
     def run(self):
         while self.quitting == False:
             self.input = self.connectionSocket.recv(1024)
             self.CommandResolve(self.input)
-            print("Closed Thread")
+            
+        self.SendReply(226)
+        print("Closed Thread")
             
     def CheckUserName(self,username):
         userData = open("LoginDetails.txt",'r')
         users = userData.readlines()
+        userFound = False
         for x in range(0,users.__len__()):
             nameAndPass = users[x].split()
             if nameAndPass[0]==username:
                 self.currentUsername = username
                 self.currentUserPassword = nameAndPass[1]
+                self.SendReply(331)
+                userFound = True
                 print "User Found"
+        if userFound == False:
+            self.SendReply(530) 
+            print "User Not found"
                 
     def CheckPassword(self,password):
         if password == self.currentUserPassword:
+            self.SendReply(230)
             print "Authenticated"
         else:
+            self.SendReply(530)
             print "Incorrect Password"
 
     def Port(self,port):
         self.portNumber = port
+        self.SendReply(200)
         
     def Retrieve(self,p):
+        self.SendReply(200)
+        self.SendReply(250)
         print p
         
     def Store(self,p):
+        self.SendReply(200)
+        self.SendReply(250)
         print p
         
     def OkServer(self):
         print "OK Server"
         
     def Quit(self):
+        self.SendReply(221)
         self.quitting = True
     
     commands = {
@@ -66,11 +83,33 @@ class FTPThread(threading.Thread):
         try:
             self.parameter = commandParameter
             self.commands.get(commandCode)(self)
-        except KeyError:
+        except (KeyError, TypeError):
+            self.SendReply(500)
             print "Invalid Command" 
+        
+            
+    replies = {
+            150: "150 File status okay; about to open data connection.",
+            200: "200 Command okay.",
+            202: "202 Command not implemented, superfluous at this site.",
+            220: "220 Service ready for new user",    
+            221: "221 Service closing control connection. Logged out.",
+            226:	"226 Closing data connection. Requested file action successful",
+            230: "230 User logged in, proceed.",
+            250: "250 Requested file action okay, completed.",
+            331: "331 User name okay, need password.",
+            425:	"425 Can't open data connection",
+            500: "500 Syntax error, command unrecognized",
+            530: "530 Not logged in or log in details incorrect",
+            550: "550 Requested action not taken. File unavailable"
+     
+            }
+    
+    def SendReply(self, code):
+        message = self.replies.get(code)(self)
+        self.connectionSocket.send(message)
 
 
 thread = FTPThread(1,1000)
 #thread.start() Uncomment when the thread must recieve continuously
 thread.CommandResolve("USER Tev")
-thread.CommandResolve("PASS Pass1")
