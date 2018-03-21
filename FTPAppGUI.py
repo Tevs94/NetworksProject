749ba6bd44e9934ca42600b26c0b5aa5e3d46347
Tev <tevia.sapire@gmail.com>
2018-03-21 18:40:00 +0200

    First Iteration of Login Screen GUI

--- FTPAppGUI.py
+++ FTPAppGUI.py
@@ -1,26 +1,38 @@
 from Tkinter import *
 
-root = Tk()
-root.geometry("300x200")
-root.resizable(0, 0)
-
+window = Tk()
+window.resizable(0, 0)
+frame = Frame(window)
 class LoginScreen:
-    def __init__(self,root):
-        self.window = root
+    def __init__(self,window):
+        self.window = window
+        self.window.geometry("300x150")
         self.window.title("Enter your credentials")
-        self.usernameLabel = Label(self.window,text = "Username")
-        self.passwordLabel = Label(self.window,text = "Password")
-        self.usernameLabel.grid(row = 1,sticky = SE,padx = 30,pady = 10)
-        self.passwordLabel.grid(row = 2,sticky = SE,padx = 30,pady = 10)
+        self.usernameLabel = Label(self.window,text = "Username:")
+        self.passwordLabel = Label(self.window,text = "Password:")
+        self.usernameLabel.grid(row = 1,padx = 30,pady = 10)
+        self.passwordLabel.grid(row = 2,padx = 30,pady = 10)
         
         self.usernameEntry = Entry(self.window)
         self.passwordEntry = Entry(self.window)
-        self.usernameEntry.grid(row = 1,column = 1,sticky = N)
-        self.passwordEntry.grid(row = 2,column = 1,sticky = N)
+        self.usernameEntry.grid(row = 1,column = 1,padx = 10,pady = 10)
+        self.passwordEntry.grid(row = 2,column = 1,padx = 10,pady = 10)
         
         submitButton = Button(self.window, text = "Login")
-        submitButton.grid(row = 3, column = 1,sticky = N)
+        submitButton.grid(row = 3, column = 1,columnspan = 2)
         
-
-Login = LoginScreen(root)
-root.mainloop()
+class FileExploreScreen:
+    def __init__(self, window):
+        self.window = window
+        self.window.geometry("300x150")
+        self.window.title("Download or Upload Files")     
+        
+        uploadButton = Button(self.window, text = "Upload A File")
+        uploadButton.grid(row = 1, column = 1, padx = 20,pady = 20)
+        
+        downloadButton = Button(self.window, text = "Download A File")
+        downloadButton.grid(row = 1, column = 2, padx = 20,pady = 20)
+        
+        
+Screen = LoginScreen(root)
+window.mainloop()
